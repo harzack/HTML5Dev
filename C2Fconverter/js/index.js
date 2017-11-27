0f8fb4899d0bc490b0ff8221c4101b6883d04546
Alex Pierre <harzack86@gmail.com>
2017-11-26 22:50:45 -0500

    minor changes to functions and styles
    added other conversions (knots and weights), and used flex css style to fit the window.

--- C2Fconverter/js/index.js
+++ C2Fconverter/js/index.js
@@ -17,3 +17,27 @@ function convertF2C() {
 	var fTemp = document.getElementById("fahrenheit").value;
 	cTemp.value = ((fTemp - 32) * (5 / 9)).toFixed(1);	
 }
+
+function convertK2P() {
+	var kWeight = document.getElementById("kilogram").value;
+	var pWeight = document.getElementById("pound");
+	pWeight.value = (kWeight / 0.45359237).toFixed(1);	
+}
+
+function convertP2K() {
+	var pWeight = document.getElementById("pound").value;
+	var kWeight = document.getElementById("kilogram");
+	kWeight.value = (pWeight * 0.45359237).toFixed(1);	
+}
+
+function convertK2N() {
+	var kSpeed = document.getElementById("kmh").value;
+	var nSpeed = document.getElementById("noeuds");
+	nSpeed.value = (kSpeed / 1.852).toFixed(1);	
+}
+
+function convertN2K() {
+	var nSpeed = document.getElementById("noeuds").value;
+	var kSpeed = document.getElementById("kmh");
+	kSpeed.value = (nSpeed * 1.852).toFixed(1);	
+}
